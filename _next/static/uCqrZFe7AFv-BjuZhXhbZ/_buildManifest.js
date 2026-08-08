df0ac1b810fd2a2394047e79ce660af2421d7f2b
ReDLiX123 <andrej.krekhov@yandex.ru>
2026-08-09 03:32:15 +0800

    Updates

--- _next/static/uCqrZFe7AFv-BjuZhXhbZ/_buildManifest.js
+++ _next/static/uCqrZFe7AFv-BjuZhXhbZ/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/tuttogusto//_next/:path+",
+        "destination": "/tuttogusto/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
